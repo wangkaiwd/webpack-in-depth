13b10dbb32d69c35ccdc0dddae305ba7fbb3d2fa
wangkaiwd <1162486176@qq.com>
2019-06-26 23:23:07 +0800

    feat: 成功使用css

--- webpack-demo/src/main.js
+++ webpack-demo/src/main.js
@@ -1,9 +1,11 @@
 import demo1 from './demo1';
 import demo2 from './demo2';
 import avatar from './avatar.jpeg';
+import './main.css';
 
 console.log('info', demo1, demo2, avatar);
 const img = document.createElement('img');
 img.src = `./dist/${avatar}`;
+img.classList.add('avatar');
 document.body.appendChild(img);
 
