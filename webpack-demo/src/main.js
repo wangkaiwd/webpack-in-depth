9084a14e3ff305c1fc17033fc013122c086a850c
wangkaiwd <1162486176@qq.com>
2019-06-27 21:02:15 +0800

    feat: 项目中使用scss文件，webpack配置遗留问题修复

--- webpack-demo/src/main.js
+++ webpack-demo/src/main.js
@@ -1,7 +1,7 @@
 import demo1 from './demo1';
 import demo2 from './demo2';
 import avatar from './avatar.jpeg';
-import './main.css';
+import './main.scss';
 
 console.log('info', demo1, demo2, avatar);
 const img = document.createElement('img');
